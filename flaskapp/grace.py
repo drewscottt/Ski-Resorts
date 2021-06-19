eee6066d96e7790ccd1300b0df0edea27fabdf8c
drewscottt <drewscott1103@gmail.com>
2021-06-19 20:24:16 +0000

    making devel cuz i'm lazy

--- flaskapp/grace.py
+++ flaskapp/grace.py
@@ -9,10 +9,12 @@ from main import app
 @app.route("/grace", methods=['GET', 'POST'])
 def grace():
     conn = MySQLdb.connect(user=GRACE_USER, password=GRACE_PASSWORD, host='localhost', database=DB, auth_plugin='mysql_native_password')
+    cursor = conn.cursor()
+
+    cookie_token = get_cookie_info(cursor, request)
 
     if request.method == 'POST':
         if len(request.form) == 2:
-            cursor = conn.cursor()
             query = 'INSERT INTO grace_comments (message, user, time) VALUES(%s, %s, %s)'
 
             now = datetime.now()
@@ -20,25 +22,37 @@ def grace():
             user = request.form['user']
 
             cursor.execute(query, (message, user, now))
-            cursor.close()
-            conn.commit()
         else:
-            cursor = conn.cursor()
             query = 'DELETE FROM grace_comments WHERE id=%s'
 
             comment_id = request.form['id']
             cursor.execute(query, (comment_id,))
 
-            cursor.close()
-            conn.commit()
-
-    cursor = conn.cursor()
     query = 'SELECT * FROM grace_comments'
     cursor.execute(query)
     comments = cursor.fetchall()
+
     cursor.close()
+    conn.commit()
+    conn.close()
 
     if request.method == 'POST':
-        return redirect(url_for("grace"))
+        response = make_response(redirect(url_for("grace")))
+    else:
+        response = make_response(render_template("grace.html", comments=comments))
 
-    return render_template("grace.html", comments=comments)
+    response.set_cookie('grace_cookie', cookie_token)
+
+    return response
+
+def get_cookie_info(cursor, request):
+    if 'grace_cookie' in request.cookies:
+        cookie_token = request.cookies['grace_cookie']
+    else:
+        cookie_token = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+        
+
+    return cookie_token
+
+
+
